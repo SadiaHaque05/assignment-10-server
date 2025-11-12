4c7557f8d39570fb203049cbb632bab14bbb9084
SadiaHaque05 <sadiahaque5403@gmail.com>
2025-11-13 04:59:17 +0600

    added delete functionalities

--- index.js
+++ index.js
@@ -104,6 +104,12 @@ async function run() {
       res.send(favorites);
     });
 
+    app.delete("/arts/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await artsCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
     app.put("/arts/:id", async (req, res) => {
       const id = req.params.id;
       const updatedArt = req.body;
